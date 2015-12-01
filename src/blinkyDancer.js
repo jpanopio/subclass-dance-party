3efd79725b9d16b4d9ad883b7309eaf9b7c33934
jpanopio <jcpanopio1@gmail.com>
2015-11-30 21:00:29 -0800

    Changes formula for calculating movement

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -16,9 +16,9 @@ makeBlinkyDancer.prototype.step = function() {
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    var direction = String(Math.floor((Math.random()*2 - 1)*10));
-    var animations = ["left","top"];
-    var animater = animations[Math.floor(Math.random()*2)];
+    var direction = String(Math.floor(Math.random()*500));
+    var animations = ["left","top","right","bottom"];
+    var animater = animations[Math.floor(Math.random()*4)];
 
     this.$node.css(animater,direction + "px");
 };
